d7e5a83f8619fc3462681d7dad69cd4949c05231
danimad <dani.madvillain@gmail.com>
2015-11-22 21:16:22 +0100

    templates

---
+++ src/common.h
@@ -0,0 +1,14 @@
+//
+//  common.h
+//  onlab_num
+//
+//  Created by Berecz Dániel on 2015.11.22..
+//  Copyright (c) 2015 Berecz Dániel. All rights reserved.
+//
+
+#ifndef onlab_num_common_h
+#define onlab_num_common_h
+
+
+
+#endif
